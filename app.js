d007ae0b361334cf1fe2bfdd8bb490bc6d244762
Western1833 <145915895+Western1833@users.noreply.github.com>
2025-03-13 23:51:09 +0200

    testing server and adding nodemon

--- app.js
+++ app.js
@@ -3,4 +3,8 @@ const port = 5050;
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello from server side');
+});
+
 app.listen(port, () => console.log(`Server is listening on port ${port}...`));
