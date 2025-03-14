c01a1983b0e9f663effaddc2314df0adc849c70e
Western1833 <145915895+Western1833@users.noreply.github.com>
2025-03-14 18:58:31 +0200

    added routes to tourRouter.js

--- app.js
+++ app.js
@@ -1,89 +1,15 @@
-const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
 
+const tourRouter = require('./routes/tourRoutes');
+
 
 const port = 5050;
-
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
-
-const getAllTours = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tours
-        }
-    });
-};
-
-const getSingleTour = (req, res) => {
-    const id = Number(req.params.id);
-
-    const tour = tours.find(el = el.id === id);
-
-    if(!tour){
-        res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    };
-};
-
-const createTour = (req, res) => {
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({id: newId}, req,body);
-
-    tours.push(newTour);
-
-    fs.writeFile(
-        `${__dirname}/dev-data/data/tours-simple.json`,
-        JSON.stringify(tours),
-        err => {
-            res.status(204).json({
-                status: 'success',
-                data: {
-                    tour: newTour
-                }
-            });
-        }
-    );
-};
-
-const updateTour = (req, res) => {
-    if(Number(req.params.id > tours.length)){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: '<updated tour here...'
-        }
-    });
-};
-
-const deleteTour = (req, res) => {
-    if(Number(req.params.id > tours.length)){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-
-    res.status(204).json({
-        status: 'success',
-        data: null
-    });
-};
 
 const getAllUsers = (req, res) => {
     res.status(500).json({
@@ -120,17 +46,7 @@ const deleteUser = (req, res) => {
     });
 }
 
-const tourRouter = express.Router();
 const userRouter = express.Router();
-
-tourRouter.route('/')
-.get(getAllTours)
-.post(createTour);
-
-tourRouter.route('/:id')
-.get(getSingleTour)
-.patch(updateTour)
-.delete(deleteTour);
 
 userRouter.route('/')
 .get(getAllUsers)
