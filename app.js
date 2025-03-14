676dda0e57bebcb1f11254801c145717661622ef
Western1833 <145915895+Western1833@users.noreply.github.com>
2025-03-14 19:25:54 +0200

    transfer app and server to server.js
    plus fixing the name to start server with nodemon with server.js

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-
-const port = 5050;
-
 const app = express();
 
 app.use(morgan('dev'));
@@ -15,4 +12,4 @@ app.use(express.json());
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+module.exports = app;
