cb31e2ab2c62ba300e8297fdf26f6a32d6e0a80a
Western1833 <145915895+Western1833@users.noreply.github.com>
2025-03-16 13:46:33 +0200

    connect db

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app.js');
+
 dotenv.config({path: './config.env'});
 
-const app = require('./app.js');
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 5050;
 
