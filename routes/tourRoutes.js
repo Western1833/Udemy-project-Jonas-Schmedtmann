06004380c11f4780850471a5b672f8c86fdb4346
Western1833 <145915895+Western1833@users.noreply.github.com>
2025-03-24 23:51:25 +0200

    Another aggregation pipeline added

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 // router.param('id', tourController.checkId);
 router.route('/top-5').get(tourController.aliasTopTours, tourController.getAllTours);
 router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router.route('/')
 .get(tourController.getAllTours)
