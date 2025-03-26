ef4caa3742adbe7273e8562768176f72737fc6c8
Western1833 <145915895+Western1833@users.noreply.github.com>
2025-03-26 23:37:44 +0200

    Sending correct mongoose errors to client
    duplicate field values

--- controlers/errorController.js
+++ controlers/errorController.js
@@ -5,6 +5,12 @@ const handleCastErrorDB = (err) => {
     return new AppError(message, 400);
 }
 
+const handleDuplicateFieldsDB = (err) => {
+    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+    const message = `Duplicate field value: ${value}, please use another value!`;
+    return new AppError(message, 400);
+}
+
 const errorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -38,9 +44,8 @@ module.exports = (err, req, res, next) => {
     }else if(process.env.NODE_ENV === 'production'){
         let error = {...err};
 
-        if(error.name === 'CastError'){
-            error = handleCastErrorDB(error);
-        }
+        if(error.name === 'CastError') error = handleCastErrorDB(error);
+        if(error.code = 11000) error = handleDuplicateFieldsDB(error);
 
         errorProd(error, res);
     }
