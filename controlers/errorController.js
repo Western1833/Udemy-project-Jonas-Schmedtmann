4089b75a3b97b80b0daa93b7f1942efd4c4c16b1
Western1833 <145915895+Western1833@users.noreply.github.com>
2025-03-26 23:05:00 +0200

    Update errorController.js

--- controlers/errorController.js
+++ controlers/errorController.js
@@ -1,9 +1,34 @@
+const errorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    });
+};
+
+const errorProd = (err, res) => {
+    if(err.isOperationsl){
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        });
+    }else{
+        console.error('Error: ', err);
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went wrong!'
+        });
+    }
+};
+
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });
+    if(process.env.NODE_ENV === 'development'){
+        errorDev(err, res);
+    }else if(process.env.NODE_ENV === 'production'){
+        errorProd(err, res);
+    }
 }
