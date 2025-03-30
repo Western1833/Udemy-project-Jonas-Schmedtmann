c95403de2fc16e78826ea8403dd665093c33b3c3
Western1833 <145915895+Western1833@users.noreply.github.com>
2025-03-30 14:50:34 +0300

    Security measure for creating new user

--- controlers/authController.js
+++ controlers/authController.js
@@ -2,7 +2,13 @@ const User = require('../models/userModel.js');
 const catchAsync = require('../utils/catchAsync.js');
 
 exports.signup = catchAsync(async(req, res, next) => {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        photo: req.body.photo,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    });
 
     res.status(201).json({
         status: 'success',
