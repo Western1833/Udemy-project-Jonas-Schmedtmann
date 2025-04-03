cfe8f79f94e6ca0f1d630aa01cc9987048ae7918
Western1833 <145915895+Western1833@users.noreply.github.com>
2025-04-03 21:04:48 +0300

    Password reset token

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -30,7 +31,9 @@ const userSchema = new mongoose.Schema({
     },
     passwordChangedAt: {
         type: Date
-    }
+    },
+    passwordResetToken: String,
+    passwordResetExpires: Date
 });
 
 userSchema.virtual('passwordConfirm')
@@ -71,6 +74,16 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
 
     return JWTTimestamp < changedTimestamp;
 };
+
+userSchema.methods.createPasswordResetToken = function() {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+
+    this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+    console.log({resetToken}, this.passwordResetToken);
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+    return resetToken;
+}
 
 const User = mongoose.model('User', userSchema);
 
