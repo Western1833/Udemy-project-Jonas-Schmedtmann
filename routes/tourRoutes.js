5f76d91d55014ccec366cb11d20e8c110b9d8b73
Western1833 <145915895+Western1833@users.noreply.github.com>
2025-04-12 22:00:42 +0300

    Adding missing auth and authorization

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -12,15 +12,15 @@ router.use('/:tourId/reviews', reviewRouter)
 // router.param('id', tourController.checkId);
 router.route('/top-5').get(tourController.aliasTopTours, tourController.getAllTours);
 router.route('/tour-stats').get(tourController.getTourStats);
-router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+router.route('/monthly-plan/:year').get(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.getMonthlyPlan);
 
 router.route('/')
-.get(authController.protect, tourController.getAllTours)
-.post(tourController.createTour);
+.get(tourController.getAllTours)
+.post(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.createTour);
 
 router.route('/:id')
 .get(tourController.getSingleTour)
-.patch(tourController.updateTour)
+.patch(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.updateTour)
 .delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour);
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,19 +8,24 @@ const router = express.Router();
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 
-router.route('/me').get(authController.protect, userController.getMe, userController.getSingleUser);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
-router.patch('/updateMyPassword', authController.protect, authController.updatePassword);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+
+router.use(authController.protect)
+
+router.route('/me').get(userController.getMe, userController.getSingleUser);
+router.patch('/updateMyPassword', authController.updatePassword);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
+
+router.use(authController.restrictTo('admin'));
 
 router.route('/')
-.get(userController.getAllUsers)
+.get(userController.getAllUsers);
 
 router.route('/:id')
 .get(userController.getSingleUser)
 .patch(userController.updateUser)
-.delete(authController.protect, authController.restrictTo('admin'), userController.deleteUser);
+.delete(userController.deleteUser);
 
 module.exports = router;
